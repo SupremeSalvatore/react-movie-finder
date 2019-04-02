a50b92220aca0795eb45da48864cbb34887bb16f
Supreme Salvatore <milosposkuricakg@gmail.com>
2019-04-02 19:26:44 +0200

    Updates

---
+++ precache-manifest.4a2808515af33f4aa7adc8041de47665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7b56f7327f468aa6569",
+    "url": "/react-movie-finder/static/js/runtime~main.01d3a085.js"
+  },
+  {
+    "revision": "2ea1a899567d16bd46b2",
+    "url": "/react-movie-finder/static/js/main.8917057b.chunk.js"
+  },
+  {
+    "revision": "0cadb78ab33d22f5a240",
+    "url": "/react-movie-finder/static/js/2.842a0779.chunk.js"
+  },
+  {
+    "revision": "2ea1a899567d16bd46b2",
+    "url": "/react-movie-finder/static/css/main.64fb67a5.chunk.css"
+  },
+  {
+    "revision": "ad24e0a2d3e2ecac19bb82da5578ab51",
+    "url": "/react-movie-finder/index.html"
+  }
+];
